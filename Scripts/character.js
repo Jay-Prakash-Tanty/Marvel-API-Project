ab55b980547ca2a4081743ad402b38e80ecc44cc
dharmendra-007 <mahantas025@gmail.com>
2024-07-17 22:19:11 +0530

    search bar working

--- Scripts/character.js
+++ Scripts/character.js
@@ -3,18 +3,71 @@
 
 const url = 'https://gateway.marvel.com/v1/public/characters?ts=1&apikey=705d91f0b752751608085d8bd3da3fe2&hash=10d96edf8082f8797acbb2bca03664b0';
 
-const baseUrl = "https://gateway.marvel.com/v1/public/characters"
+const baseUrl = "https://gateway.marvel.com/v1/public/characters";
 
-const key = "ts=1&apikey=705d91f0b752751608085d8bd3da3fe2&hash=10d96edf8082f8797acbb2bca03664b0"
+const key = "ts=1&apikey=705d91f0b752751608085d8bd3da3fe2&hash=10d96edf8082f8797acbb2bca03664b0";
 
-document.addEventListener("load", fetchdata()) 
+let input = document.getElementById("search-text");
 
-async function fetchdata() {
+document.addEventListener("load", fetchdata("")); 
+
+function reload() {
+  window.location.reload();
+}
+
+function search(){
+  fetchdata(input.value);
+}
+
+async function fetchdata(input) {
+  if(input == ""){
   console.log('fetching data...');
   const res = await fetch(url);
   const data = await res.json();
   console.log(data);
+  binddataShow(data.data.results);
+}
+else {
+  const query = `name=${input.replace(/ /g, "%20")}`
+  const res = await fetch(`${baseUrl}?${query}&${key}`);
+  console.log(query);
+  const data = await res.json();
+  console.log(data);
   binddata(data.data.results);
+  input.value = "";
+}
+}
+
+function binddataShow(datas) {
+  const cardsContainer = document.getElementById('card-container')
+  const CardTemplate = document.getElementById('template-char-card')
+
+  cardsContainer.innerHTML = "";
+
+  datas.forEach((data) => {
+    const regex = /^http:\/\/i\.annihil\.us\/u\/prod\/marvel\/i\/mg\/.*\/image_not_available$/;
+    if(regex.test(data.thumbnail.path)) {
+      return;
+    };
+    const cardClone = CardTemplate.content.cloneNode(true);
+    fillDataInCard(cardClone, data);
+    cardsContainer.appendChild(cardClone);
+  });
+  
+  function fillDataInCard(cardClone, data) {
+    const charImg = cardClone.querySelector('#char-img');
+    const charName = cardClone.querySelector('#char-name');
+    const charDesc = cardClone.querySelector('#char-description');
+    charImg.src = `${data.thumbnail.path}/standard_xlarge.${data.thumbnail.extension}`;
+    charName.innerHTML = data.name;
+    charDesc.innerHTML = data.description;
+    if(data.description == ""){
+      charDesc.innerHTML = "No Description Found";
+    }
+    cardClone.firstElementChild.addEventListener('click', () =>{
+      window.open(data.resourceURI, '_blank');
+    });
+  };
 }
 
 function binddata(datas) {
@@ -24,8 +77,10 @@ function binddata(datas) {
   cardsContainer.innerHTML = "";
 
   datas.forEach((data) => {
-    const regex = /^http:\/\/i\.annihil\.us\/u\/prod\/marvel\/i\/mg\/.*\/image_not_available$/;
-    if(regex.test(data.thumbnail.path)) return;
+    // const regex = /^http:\/\/i\.annihil\.us\/u\/prod\/marvel\/i\/mg\/.*\/image_not_available$/;
+    // if(regex.test(data.thumbnail.path)) {
+    //   return;
+    // };
     const cardClone = CardTemplate.content.cloneNode(true);
     fillDataInCard(cardClone, data);
     cardsContainer.appendChild(cardClone);
@@ -34,8 +89,13 @@ function binddata(datas) {
   function fillDataInCard(cardClone, data) {
     const charImg = cardClone.querySelector('#char-img');
     const charName = cardClone.querySelector('#char-name');
-    charImg.src = `${data.thumbnail.path}/portrait_fantastic.${data.thumbnail.extension}`;
-    charName.innerHTML = data.name;  
+    const charDesc = cardClone.querySelector('#char-description');
+    charImg.src = `${data.thumbnail.path}/standard_xlarge.${data.thumbnail.extension}`;
+    charName.innerHTML = data.name;
+    charDesc.innerHTML = data.description;
+    if(data.description == ""){
+      charDesc.innerHTML = "No Description Found";
+    }
     cardClone.firstElementChild.addEventListener('click', () =>{
       window.open(data.resourceURI, '_blank');
     });
