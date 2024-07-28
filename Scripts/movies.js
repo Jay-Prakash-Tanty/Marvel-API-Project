06ad7e09bd8a2f4138280fa0f129395d1cb77175
Jay-Prakash-Tanty <jayprakashtanty@gmail.com>
2024-07-28 11:35:45 +0530

    final movies chnages

--- Scripts/movies.js
+++ Scripts/movies.js
@@ -1,3 +1,48 @@
+
+// haha
+
+const searchBox = document.querySelector(".search-box");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const searchInput = document.querySelector("input");
+const sklaerch = document.querySelector(".tingting");
+
+sklaerch.classList.add("unclicked");
+
+searchBtn.onclick =()=>{
+
+    //
+    sklaerch.classList.add("fixer");
+    sklaerch.classList.remove("unclicked");
+    //
+    searchBox.classList.add("active");
+    searchBtn.classList.add("active");
+    searchInput.classList.add("active");
+    cancelBtn.classList.add("active");
+    searchInput.focus();
+
+
+        // jatra();
+
+    }
+
+cancelBtn.onclick =()=>{
+    //
+    sklaerch.classList.remove("fixer");
+    searchInput.value = "";
+    sklaerch.classList.add("unclicked");
+    //
+    searchBox.classList.remove("active");
+    searchBtn.classList.remove("active");
+    searchInput.classList.remove("active");
+    cancelBtn.classList.remove("active");
+    // searchInput.value = "";
+    }
+
+//haha
+
+
+
 const movieSearchBox = document.getElementById('movie-search-box');
 const searchList = document.getElementById('search-list');
 const resultGrid = document.getElementById('result-grid');
